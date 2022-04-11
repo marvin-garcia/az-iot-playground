35ac677bf39a74daf18ef9ef5d7834e2ab248d0b
Marvin Garcia <magar@microsoft.com>
2022-04-11 15:10:53 -0400

    updated ADT function code

--- iot-plug-and-play/digital-twins-ingestion/DigitalTwinsFunction/DigitalTwinsFunction/IoTHubtoTwins.cs
+++ iot-plug-and-play/digital-twins-ingestion/DigitalTwinsFunction/DigitalTwinsFunction/IoTHubtoTwins.cs
@@ -18,6 +18,21 @@ namespace DigitalTwinsFunction
     {
         private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
         private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly string[] deviceProperties = new[] 
+        { 
+            "Temperature",
+            "Pressure",
+            "Humidity",
+            "magnetometerX",
+            "magnetometerY",
+            "magnetometerZ",
+            "accelerometerX",
+            "accelerometerY",
+            "accelerometerZ",
+            "gyroscopeX",
+            "gyroscopeY",
+            "gyroscopeZ" 
+        };
 
         [FunctionName("IoTHubtoTwins")]
         public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
@@ -30,9 +45,10 @@ namespace DigitalTwinsFunction
                 DigitalTwinsClient client = new DigitalTwinsClient(
                     new Uri(adtInstanceUrl),
                     credentials,
-                    new DigitalTwinsClientOptions { Transport = new HttpClientTransport(httpClient) });
-
-                log.LogInformation($"ADT service client connection created.");
+                    new DigitalTwinsClientOptions 
+                    { 
+                        Transport = new HttpClientTransport(httpClient) 
+                    });
 
                 if (eventGridEvent != null && eventGridEvent.Data != null)
                 {
@@ -40,76 +56,21 @@ namespace DigitalTwinsFunction
                     string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
                     string deviceBody = (string)deviceMessage["body"];
                     JObject body = JsonConvert.DeserializeObject<JObject>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(deviceBody)));
-                    //log.LogInformation($"DeviceId: {deviceId}");
-                    //log.LogInformation($"Encoded Body: {deviceBody}");
-                    //log.LogInformation($"Decoded Body: {body}");
 
-                    // Temperature
-                    if (body.ContainsKey("temperature"))
+                    //var updateTwinData = new JsonPatchDocument();
+                    foreach (var property in body.Properties())
                     {
-                        var temperature = body["temperature"];
-                        log.LogInformation($"Device:{deviceId} Temperature is:{temperature}");
+                        log.LogInformation($"Device: {deviceId} | {property.Name}: {body.GetValue(property.Name)}");
+                        //updateTwinData.AppendReplace($"/{deviceProperty}", propertyValue.Value<double>());
 
-                        var updateTwinData = new JsonPatchDocument();
-                        updateTwinData.AppendReplace("/Temperature", temperature.Value<double>());
-
-                        await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
+                        await client.PublishTelemetryAsync(deviceId, Guid.NewGuid().ToString(), $"{{ \"{property.Name}\": {body.GetValue(property.Name).Value<double>()} }}");
                     }
 
-                    //// Humidity
-                    //if (body.ContainsKey("humidity"))
-                    //{
-                    //    var humidity = body["humidity"];
-                    //    log.LogInformation($"Device:{deviceId} humidity is:{humidity}");
-                    //    updateTwinData.AppendReplace("/Humidity", humidity.Value<double>());
-
-                    //    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                    //}
-
-                    //// Pressure
-                    //if (body.ContainsKey("pressure"))
-                    //{
-                    //    var pressure = body["pressure"];
-                    //    log.LogInformation($"Device:{deviceId} pressure is:{pressure}");
-                    //    updateTwinData.AppendReplace("/Pressure", pressure.Value<double>());
-
-                    //    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                    //}
-
-                    //// magnetometerX
-                    //if (body.ContainsKey("magnetometerX"))
-                    //{
-                    //    var magnetometerX = body["magnetometerX"];
-                    //    log.LogInformation($"Device:{deviceId} X magnetometer is:{magnetometerX}");
-                    //    updateTwinData.AppendReplace("/MagnetometerX", magnetometerX.Value<double>());
-
-                    //    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                    //}
-
-                    //// magnetometerY
-                    //if (body.ContainsKey("magnetometerY"))
-                    //{
-                    //    var magnetometerY = body["magnetometerY"];
-                    //    log.LogInformation($"Device:{deviceId} Y magnetometer is:{magnetometerY}");
-                    //    updateTwinData.AppendReplace("/MagnetometerY", magnetometerY.Value<double>());
-
-                    //    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                    //}
-
-                    //// magnetometerZ
-                    //if (body.ContainsKey("magnetometerZ"))
-                    //{
-                    //    var magnetometerZ = body["magnetometerZ"];
-                    //    log.LogInformation($"Device:{deviceId} Z magnetometer is:{magnetometerZ}");
-                    //    updateTwinData.AppendReplace("/MagnetometerZ", magnetometerZ.Value<double>());
-
-                    //    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                    //}
+                    //await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
                 }
             }
             catch (Exception ex)
             {
-                log.LogError($"Error in ingest function: {ex.Message}");
                 throw ex;
             }
         }
